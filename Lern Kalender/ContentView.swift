d2b17906eee9ebd8dec79e05169778ce84df26a0
nlslsksw <nils.lohrmann11@gmail.com>
2026-02-27 14:12:59 +0100

    Initial Commit

---
+++ Lern Kalender/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lern Kalender
+//
+//  Created by Nils on 27.02.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Lern Kalender/Lern_KalenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lern_KalenderApp.swift
+//  Lern Kalender
+//
+//  Created by Nils on 27.02.26.
+//
+
+import SwiftUI
+
+@main
+struct Lern_KalenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
